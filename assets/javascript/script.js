79ca5824f352cde771748ef8c9869015b113b676
Trevor Dorn <dorntrevor7@gmail.com>
2020-02-18 22:11:15 -0700

    forecast working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,7 +11,6 @@ var $forecast = $(".forecast")
 // Weather functions
 // Forecast function
 function currentWeather() {
-
     var searchValue = $("input").val().trim()
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=09335e290f36cab7997b39556d93e753&units=imperial";
 
@@ -24,6 +23,8 @@ function currentWeather() {
         // waiting for the ajax call to resolve
         .then(function (data) {
 
+            $("#cityResults").empty();
+
             console.log(data);
             // Creates a div to hold the city 
             var $data = $("<div>")
@@ -34,9 +35,11 @@ function currentWeather() {
             // Displays the city name
             $data.append($cityName);
             // Retrieves the time Data
-            var time = moment().subtract(10, 'days').calendar();
+            var time = moment().format('L');
+            // image icon
+            var image = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
             // appending the time to the city name
-            $cityName.append(" " + time);
+            $cityName.append(" " + time + " " + image);
             // Displays the city name
             $data.append($cityName);
             // retrieve the temp
@@ -57,24 +60,6 @@ function currentWeather() {
         });
 }
 
-// Icon: 
-// function icon () {
-
-//     var searchValue = $("input").val().trim()
-//     var queryURL = "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png"
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     })
-
-//     // waiting for the ajax call to resolve
-//     .then(function (data) {
-//         console.log(data);
-//     })
-// };
-
-
 // On any button click
 function forecast() {
 
@@ -89,17 +74,43 @@ function forecast() {
 
         // waiting for the ajax call to resolve
         .then(function (data) {
-            console.log(data);
-        })
+            $("#cast").empty();
 
+            console.log(data);
+
+            for (var i = 0; i < 5; i++) {
+  
+                var $data = $("<div>").addClass("cast");
+                var $date = $("<h5>").text(moment(data.list[i].dt).format("L"));
+                console.log(moment(data.list[i].dt).format("L"));
+                var $temp = $("<p>").text("temp: " + data.list[i].main.temp + " °F");
+                console.log(data.list[i].main.temp);
+                var $humidity = $("<p>").text("humidity: " + data.list[i].main.humidity + " %");
+                console.log(data.list[i].main.humidity);
+                $data.append(
+                    $date,
+                    $temp,
+                    $humidity
+                )
+                $("#cast").append($data);
+            }
+        })
+        
 };
 
-$("button").on("click", function () {
+$("button").on("click", function click () {
 
     var searchValue = $("input").val().trim()
     var $cityLi = $("<button>").text(searchValue);
+    $cityLi.addClass("cityButton");
     $("#searchInput").append($cityLi);
-    
+
+    currentWeather();
+    forecast();
+});
+
+$(document).on("click", ".cityButton", function (){
+
     currentWeather();
     forecast();
 });
